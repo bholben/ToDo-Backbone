d0d69fc445f62e8b93ec8e19ffd7d93520a6622d
Bob Holben <bholben@gmail.com>
2015-02-13 15:06:17 -0500

    Orgnize code into MVC sections

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,11 +4,7 @@ var app = {};
 (function () {
   'use strict';
 
-  // Cache html elements
-  var taskTemplate = Handlebars.templates.task,
-      $form = $('#form'),
-      $newTask = $('#new-task'),
-      $taskList = $('#task-list');
+  ////////////////////////////////// MODEL //////////////////////////////////
 
   // Create Task model
   app.Task = Backbone.Model.extend({
@@ -22,7 +18,7 @@ var app = {};
       app.tasks.add(this).save().done(renderNewTask.bind(this));
     },
 
-    toggleComplete: function () {
+    toggle: function () {
       this.attributes.completed = !this.attributes.completed;
       this.save();
     },
@@ -35,12 +31,63 @@ var app = {};
   // Create Tasks collection
   app.Tasks = Backbone.Collection.extend({
     model: app.Task,
-    url: 'http://tiy-atl-fe-server.herokuapp.com/collections/bobs_backbone_test'
+    url: 'http://tiy-atl-fe-server.herokuapp.com/collections/bobs_backbone_tasks'
   });
 
   // Create tasks collection instance
   app.tasks = new app.Tasks();
+  // Load in any existing server models
+  app.tasks.fetch();
 
+
+  ////////////////////////////////// CONTROL //////////////////////////////////
+
+  function targetTask(e) {
+    var $li = $(e.target).closest('li'),
+        targetID = $li.attr('data-id'),
+        model = app.tasks.findWhere({_id: targetID});
+    return {$li: $li, model: model};
+  }
+
+  var control = {
+
+    create: function (e) {
+      e.preventDefault();
+      var title = $newTask.val();
+      if (title) {
+        // Create a new model (local and server) & update the view
+        var task = new app.Task();
+        task.create(title);
+      }
+    },
+
+    toggle: function (e) {
+      // Target specific task
+      var task = targetTask(e);
+      // Update the model (local and server)
+      task.model.toggle();
+      // Update the view
+      task.$li.toggleClass('completed');
+    },
+
+    delete: function (e) {
+      // Target specific task
+      var task = targetTask(e);
+      // Update the model (local and server)
+      task.model.delete();
+      // Update the view
+      task.$li.addClass('hidden');
+    }
+  };
+
+
+  ////////////////////////////////// VIEW //////////////////////////////////
+
+  // Cache html elements
+  var taskTemplate = Handlebars.templates.task,
+      $form = $('#form'),
+      $newTask = $('#new-task'),
+      $taskList = $('#task-list');
 
   // Render the new task at the top of the list and clear the input box
   function renderNewTask(obj) {
@@ -49,44 +96,9 @@ var app = {};
   }
 
   // Bind events
-  $form.on('submit', createController.bind(this));
-  $taskList.on('click', 'input.toggle', toggleController.bind(this));
-  $taskList.on('click', 'button.destroy', deleteController.bind(this));
-
-  function createController(e) {
-    e.preventDefault();
-    var title = $newTask.val();
-    if (title) {
-      // Create a new model (local and server) & update the view
-      var task = new app.Task();
-      task.create(title);
-    }
-  }
-
-  function toggleController(e) {
-    // Target specific task
-    var task = targetTask(e);
-    // Update the model (local and server)
-    task.model.toggleComplete();
-    // Update the view
-    task.$li.toggleClass('completed');
-  }
-
-  function deleteController(e) {
-    // Target specific task
-    var task = targetTask(e);
-    // Update the model (local and server)
-    task.model.delete();
-    // Update the view
-    task.$li.addClass('hidden');
-  }
-
-  function targetTask(e) {
-    var $li = $(e.target).closest('li'),
-        targetID = $li.attr('data-id'),
-        model = app.tasks.findWhere({_id: targetID});
-    return {$li: $li, model: model};
-  }
+  $form.on('submit', control.create.bind(this));
+  $taskList.on('click', 'input.toggle', control.toggle.bind(this));
+  $taskList.on('click', 'button.destroy', control.delete.bind(this));
 
 }());
 
